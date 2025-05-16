7e067f638a3de8fd47e59563aae143c977f0fa63
Luce-22 <kihikalucy059@gmail.com>
2025-05-16 23:14:21 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// Declaring a variable Traditionally
+// var name ="Excentric Productions";
+// console.log(name);
+
+// Declaring a variable (Modern)
+// let age =30;
+// console.log(age); 
+
+// const pi = 3.14;
+// console.log(pi);
+
+//Data Types
+
+//Non primitive data type. complex data type to store multiple values
+/* 1. Object - Collection of key value pairs
+let person = {
+    name: "Excetric Productions",
+    age: 19,
+    isStudent: true
+};
+console.log(person);
+
+let car ={
+    model: "BMW",
+    year: 2020,
+    color: "Turquoise"
+};
+console.log(car.color)
+*/
+
+
+/* 2 Array - An ordered list of values (Can be of diffferent data types)
+let colors = ["red", "green", "blue"];
+console.log(colors);
+console.log(colors[0]);
+*/
+
+// Functions -A block of code that can be reused
+// function greet(name){
+//     return "Hello, " + name + "!";
+// };
+// console.log(greet("Excentric"));
+
+
+function details(name, adress){
+    return "Name is, " + name +"\n" + "Email Adress is, " + adress + "!";
+};
+console.log(details("Excentric", "Xcentric@gmail.com"));
+
